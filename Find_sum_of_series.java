016ae16be540c34fff2e2a4fdcbe1b7611b0d90c
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2022-07-31 21:40:46 +0530

    import java.util.Scanner;
    class java
    {
        public static void main(String args[])
        {
            float n,i;
            Scanner sc=new Scanner(System.in);
            n=sc.nextFloat();
            float s=0;
            for(i=1;i<=n;i++)
                s=s+(float)(1/i);
            System.out.printf("%.2f",s);
        }
    }

---
+++ Find_sum_of_series.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+
+class java
+{
+    public static void main(String args[])
+    {
+        float n,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextFloat();
+        float s=0;
+        for(i=1;i<=n;i++)
+            s=s+(float)(1/i);
+        System.out.printf("%.2f",s);
+    }
+}
