86a8f14532f9e5bf212c97736c106662ead9d32c
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2022-07-31 21:20:21 +0530

    import java.util.Scanner;
    class the
    {
        public static void main(String args[])
        {
            int n;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
        if(n<=1)
        {
            System.out.printf("0");
        }
        else if(n==2)
        {
            System.out.printf("1");
        }
        else
        {
            System.out.printf("2");
        }
        }
    }

---
+++ The_Pandemic.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Scanner;
+
+class the
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+    if(n<=1)
+    {
+        System.out.printf("0");
+    }
+    else if(n==2)
+    {
+        System.out.printf("1");
+    }
+    else
+    {
+        System.out.printf("2");
+    }
+    }
+}
