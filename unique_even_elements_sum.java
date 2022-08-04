3f7d0ba0e5d39713721f8ce00a21001a7d5a37d5
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2022-08-04 21:05:40 +0530

    import java.util.Scanner;
    class java
    {
        public static void main(String args[])
        {
            int n,i,j;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            int []arr=new int[n];
            for(i=0;i<n;i++)
                arr[i]=sc.nextInt();
            int c,s=0,rep;
            for(i=0;i<n;i++)
            {
                c=0;
                rep=0;
                for(j=0;j<n;j++)
                {
                    if(j<i && arr[i]==arr[j])
                    {
                        rep=1;
                    }
                    if(arr[i]==arr[j]&& i!=j)
                    {
                        c=c+1;
                    }
                }
                if(arr[i]%2==0 && rep==0)
                {
                    s=s+arr[i];
                }
            }
            System.out.printf("%d",s);
        }
    }

---
+++ unique_even_elements_sum.java
@@ -0,0 +1,38 @@
+
+
+import java.util.Scanner;
+
+class java
+{
+    public static void main(String args[])
+    {
+        int n,i,j;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int []arr=new int[n];
+        for(i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int c,s=0,rep;
+        for(i=0;i<n;i++)
+        {
+            c=0;
+            rep=0;
+            for(j=0;j<n;j++)
+            {
+                if(j<i && arr[i]==arr[j])
+                {
+                    rep=1;
+                }
+                if(arr[i]==arr[j]&& i!=j)
+                {
+                    c=c+1;
+                }
+            }
+            if(arr[i]%2==0 && rep==0)
+            {
+                s=s+arr[i];
+            }
+        }
+        System.out.printf("%d",s);
+    }
+}
