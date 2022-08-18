2306e176ed14484c786c5f77ed553f26f6cf11ed
Sravanthi712 <96292238+Sravanthi712@users.noreply.github.com>
2022-08-18 20:17:36 +0530

    import java.util.Scanner;
    class java
    {
        public static void main(String args[])
        {
            int a,b,c;
            Scanner sc=new Scanner(System.in);
            a=sc.nextInt();
            b=sc.nextInt();
            c=sc.nextInt();
            if(a>50 && b>60 && c>100)
            {
                System.out.printf("10");
            }
            else if(a>50 && b>60 && c<=100)
            {
                System.out.printf("9");
            }
            else if(a<=50 && b>60 && c>100)
            {
                System.out.printf("8");
            }
            else if(a>50 && b<=60 && c>100)
            {
                System.out.printf("7");
            }
            else if(a>50 && b<=60 && c<=100)
            {
                System.out.printf("6");
            }
            else if(a<=50 && b>60 && c<=100)
            {
                System.out.printf("6");
            }
            else if(a<=50 && b<=60 && c>100)
            {
                System.out.printf("6");
            }
            else if(a<=50 && b<=60 && c<=100)
            {
                System.out.printf("5");
            }
        }
    }

---
+++ Grades_of_Rides.java
@@ -0,0 +1,48 @@
+
+
+import java.util.Scanner;
+
+class java
+{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        if(a>50 && b>60 && c>100)
+        {
+            System.out.printf("10");
+        }
+        else if(a>50 && b>60 && c<=100)
+        {
+            System.out.printf("9");
+        }
+        else if(a<=50 && b>60 && c>100)
+        {
+            System.out.printf("8");
+        }
+        else if(a>50 && b<=60 && c>100)
+        {
+            System.out.printf("7");
+        }
+        else if(a>50 && b<=60 && c<=100)
+        {
+            System.out.printf("6");
+        }
+        else if(a<=50 && b>60 && c<=100)
+        {
+            System.out.printf("6");
+        }
+        else if(a<=50 && b<=60 && c>100)
+        {
+            System.out.printf("6");
+        }
+        else if(a<=50 && b<=60 && c<=100)
+        {
+            System.out.printf("5");
+        }
+    }
+}
+
